03df84332bb6abc1e655e7df4867a196fa681051
FullonGeodesy <fullongeodesy@gmail.com>
2023-09-19 10:39:12 +0300

    first comit

---
+++ data.js
@@ -0,0 +1,325 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-intrare",
+      "name": "Intrare",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 1.4838847706916152,
+        "pitch": 0.03355044186063161,
+        "fov": 1.3662485491026943
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9954270748598759,
+          "pitch": 0.1585030199627635,
+          "rotation": 0,
+          "target": "1-hol"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-hol",
+      "name": "Hol",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 0.8694177908262741,
+        "pitch": -0.05745596453874313,
+        "fov": 1.3662485491026943
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.891097825242845,
+          "pitch": 0.06948196921613814,
+          "rotation": 0,
+          "target": "0-intrare"
+        },
+        {
+          "yaw": -1.30264388645573,
+          "pitch": -0.13601102236936669,
+          "rotation": 5.497787143782138,
+          "target": "6-dormitor-1"
+        },
+        {
+          "yaw": 0.73750441328942,
+          "pitch": 0.2530294505003887,
+          "rotation": 0,
+          "target": "2-sufragerie"
+        },
+        {
+          "yaw": 1.4244334215921448,
+          "pitch": 0.2840743089250015,
+          "rotation": 0,
+          "target": "3-baie"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-sufragerie",
+      "name": "Sufragerie",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 0.28059427191040953,
+        "pitch": 0.01677522093032202,
+        "fov": 1.3662485491026943
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9390930430681763,
+          "pitch": 0.0907588473640697,
+          "rotation": 0,
+          "target": "1-hol"
+        },
+        {
+          "yaw": 0.853014114083912,
+          "pitch": 0.29246024703522977,
+          "rotation": 0,
+          "target": "4-bucatarie"
+        },
+        {
+          "yaw": -2.5473872977748275,
+          "pitch": -0.1900095171935856,
+          "rotation": 5.497787143782138,
+          "target": "6-dormitor-1"
+        },
+        {
+          "yaw": 2.758868279377986,
+          "pitch": 0.1633613131031737,
+          "rotation": 10.995574287564278,
+          "target": "3-baie"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-baie",
+      "name": "Baie",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -1.7742369686212918,
+        "pitch": 0.11951854635569426,
+        "fov": 1.3662485491026943
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7143975605543851,
+          "pitch": 0.26535064717052137,
+          "rotation": 0,
+          "target": "1-hol"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-bucatarie",
+      "name": "Bucatarie",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 2.594479661160019,
+        "pitch": 0.05996486005056312,
+        "fov": 1.3662485491026943
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6667043797123693,
+          "pitch": 0.2217559915084344,
+          "rotation": 0,
+          "target": "2-sufragerie"
+        },
+        {
+          "yaw": -1.5300330397610526,
+          "pitch": -0.14249092082170556,
+          "rotation": 5.497787143782138,
+          "target": "5-dormitor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-dormitor",
+      "name": "Dormitor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -1.500972457389084,
+        "pitch": 0.12102043042447796,
+        "fov": 1.3662485491026943
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.160699916751053,
+          "pitch": 0.1382790963999554,
+          "rotation": 2.356194490192345,
+          "target": "6-dormitor-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-dormitor-1",
+      "name": "Dormitor 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": -0.0967102353864604,
+        "pitch": 0.0910097373528167,
+        "fov": 1.3662485491026943
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.0967102353864604,
+          "pitch": 0.0910097373528167,
+          "rotation": 0,
+          "target": "5-dormitor"
+        },
+        {
+          "yaw": -2.3312131295099956,
+          "pitch": 0.3453585846885687,
+          "rotation": 0,
+          "target": "2-sufragerie"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Apartament Milano",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
